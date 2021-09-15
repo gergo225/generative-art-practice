39b0f7f1a45eafee0dc2c572c48c576c4db4c760
gergo225 <fazekasgergo225@gmail.com>
2021-09-15 11:20:41 +0300

    Add diagonal line randomly and fix size again

--- 1_tiled_lines/tiled_lines.js
+++ 1_tiled_lines/tiled_lines.js
@@ -1,9 +1,10 @@
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
-let size = window.innerHeight;
+let size = 420;
 let dpr = window.devicePixelRatio;
-
+canvas.width = size * dpr;
+canvas.height = size * dpr;
 context.scale(dpr, dpr);
 
 context.lineCap = 'square';
@@ -14,8 +15,16 @@ context.fillRect(0, 0, size, size);
 context.fill();
 
 function draw(x, y, width, height) {
-	context.moveTo(x, y);
-	context.lineTo(x + width, y + height);
+	let leftToRight = Math.random() >= 0.5;
+
+	if (leftToRight) {
+		context.moveTo(x, y);
+		context.lineTo(x + width, y + height);
+	} else {
+		context.moveTo(x + width, y);
+		context.lineTo(x, y + height);
+	}
+
 	context.stroke();
 }
 
