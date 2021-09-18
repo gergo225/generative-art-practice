c7757e790357d6aab923b12f78126d63125f04f8
gergo225 <fazekasgergo225@gmail.com>
2021-09-18 13:33:57 +0300

    Draw straight lines

--- 2_joy_division/joy_division.js
+++ 2_joy_division/joy_division.js
@@ -12,3 +12,31 @@ context.lineWidth = 2;
 
 context.fillStyle = 'orange';
 context.fillRect(0, 0, size, size);
+
+let step = 10;
+let lines = [];
+
+// set up lines
+for (let y = step; y <= size - step; y += step) {
+	let line = [];
+
+	for (let x = step; x <= size - step; x += step) {
+		let point = { x: x, y: y };
+		line.push(point);
+	}
+
+	lines.push(line);
+}
+
+
+// draw lines
+for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+	context.beginPath();
+	context.moveTo(lines[lineIndex][0].x, lines[lineIndex][0].y);
+
+	for (let linePointIndex = 0; linePointIndex < lines[lineIndex].length; linePointIndex++) {
+		context.lineTo(lines[lineIndex][linePointIndex].x, lines[lineIndex][linePointIndex].y);
+	}
+
+	context.stroke();
+}
