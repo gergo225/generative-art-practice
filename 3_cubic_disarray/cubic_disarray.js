a9f075b33a4e916177942aeef621476df5281439
gergo225 <fazekasgergo225@gmail.com>
2021-09-20 22:47:01 +0300

    Add grid of cubes

--- 3_cubic_disarray/cubic_disarray.js
+++ 3_cubic_disarray/cubic_disarray.js
@@ -13,3 +13,18 @@ let squareSize = 30;
 
 context.fillStyle = 'lightgreen';
 context.fillRect(0, 0, size, size);
+
+function draw(width, height) {
+	context.beginPath();
+	context.rect(-width / 2, -height / 2, width, height);
+	context.stroke();
+}
+
+for (let i = squareSize; i <= size - squareSize; i += squareSize) {
+	for (let j = squareSize; j <= size - squareSize; j += squareSize) {
+		context.save();
+		context.translate(i, j);
+		draw(squareSize, squareSize);
+		context.restore();
+	}
+}
