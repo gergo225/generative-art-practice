20d2f26fa3ee1c7a9094d9c6645d480f7026de83
gergo225 <fazekasgergo225@gmail.com>
2021-09-21 09:44:22 +0300

    Apply random translations and rotations (add disarray)

--- 3_cubic_disarray/cubic_disarray.js
+++ 3_cubic_disarray/cubic_disarray.js
@@ -20,11 +20,25 @@ function draw(width, height) {
 	context.stroke();
 }
 
-for (let i = squareSize; i <= size - squareSize; i += squareSize) {
-	for (let j = squareSize; j <= size - squareSize; j += squareSize) {
+
+let randomDisplacement = 15;
+let rotateMultiplier = 20;
+let offset = 0;
+
+for (let i = 2 * squareSize; i <= size - 2 * squareSize; i += squareSize) {
+	for (let j = 2 * squareSize; j <= size - 2 * squareSize; j += squareSize) {
+		let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+		let rotateAmount = j / size * Math.PI / 180 * plusOrMinus * Math.random() * rotateMultiplier;
+
+		plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+		let translateAmount = j / size * plusOrMinus * Math.random() * randomDisplacement;
+
+
 		context.save();
-		context.translate(i, j);
+		context.translate(i + translateAmount + offset, j + offset);
+		context.rotate(rotateAmount);
 		draw(squareSize, squareSize);
 		context.restore();
 	}
 }
+
