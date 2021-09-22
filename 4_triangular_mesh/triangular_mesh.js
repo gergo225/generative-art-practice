99d340c11ee52aaee4e87a074aba33c7cf2466b2
gergo225 <fazekasgergo225@gmail.com>
2021-09-22 13:48:53 +0300

    Draw regular triangles

--- 4_triangular_mesh/triangular_mesh.js
+++ 4_triangular_mesh/triangular_mesh.js
@@ -21,11 +21,37 @@ for (let y = gap / 2; y <= size; y += gap) {
 	odd = !odd;
 
 	for (let x = gap / 4; x <= size; x += gap) {
-		dot = { x: x + (odd ? gap/2 : 0), y: y };
+		dot = { x: x + (odd ? gap / 2 : 0), y: y };
 		line.push(dot);
 		context.beginPath();
 		context.arc(dot.x, dot.y, 1, 0, 2 * Math.PI, true);
 		context.fill();
 	}
 	lines.push(line);
+}
+
+
+function drawTriangle(pointA, pointB, pointC) {
+	context.beginPath();
+	context.moveTo(pointA.x, pointA.y);
+	context.lineTo(pointB.x, pointB.y);
+	context.lineTo(pointC.x, pointC.y);
+	context.lineTo(pointA.x, pointA.y);
+	context.closePath();
+	context.stroke();
+}
+
+odd = true;
+for (let lineIndex = 0; lineIndex < lines.length - 1; lineIndex++) {
+	odd = !odd;
+	let combinedLine = [];
+
+	for (let i = 0; i < lines[lineIndex].length; i++) {
+		combinedLine.push(odd ? lines[lineIndex][i] : lines[lineIndex + 1][i]);
+		combinedLine.push(odd ? lines[lineIndex + 1][i] : lines[lineIndex][i]);
+	}
+
+	for (let i = 0; i < combinedLine.length - 2; i++) {
+		drawTriangle(combinedLine[i], combinedLine[i + 1], combinedLine[i + 2]);
+	}
 }
