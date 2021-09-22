987f38db65aa040e6f436a52e6ecbf5af70536ba
gergo225 <fazekasgergo225@gmail.com>
2021-09-22 13:15:12 +0300

    Add simple point grid

--- 4_triangular_mesh/triangular_mesh.js
+++ 4_triangular_mesh/triangular_mesh.js
@@ -10,3 +10,18 @@ context.lineJoin = 'bevel';
 
 context.fillStyle = 'pink';
 context.fillRect(0, 0, size, size);
+
+context.fillStyle = 'purple';
+let gap = size / 7;
+let lines = [];
+
+for (let y = gap / 2; y <= size; y += gap) {
+	let line = [];
+	for (let x = gap / 2; x <= size; x += gap) {
+		line.push({ x: x, y: y });
+		context.beginPath();
+		context.arc(x, y, 1, 0, 2 * Math.PI, true);
+		context.fill();
+	}
+	lines.push(line);
+}
