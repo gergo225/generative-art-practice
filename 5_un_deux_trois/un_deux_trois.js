22528036deaa98810cba3497d008a1fb58ffec5e
gergo225 <fazekasgergo225@gmail.com>
2021-09-23 19:23:48 +0300

    Add random rotation to little suqare parts

--- 5_un_deux_trois/un_deux_trois.js
+++ 5_un_deux_trois/un_deux_trois.js
@@ -19,7 +19,9 @@ let thirdOfHeight = size / 3;
 // 'positions' - where the line should be inside a small cube [0, 1]
 function draw(x, y, width, height, positions) {
 	context.save();
-	context.translate(x, y);
+	context.translate(x + width / 2, y + height / 2);	// to rotate from middle of object
+	context.rotate(Math.random() * 5);
+	context.translate(-width / 2, -height / 2);	// go back from middle of object
 
 	for (let i = 0; i < positions.length; i++) {
 		context.beginPath();
@@ -31,6 +33,7 @@ function draw(x, y, width, height, positions) {
 	context.restore();
 }
 
+// add complexity as we go down
 for (let y = step; y < size - step; y += step) {
 	for (let x = step; x < size - step; x += step) {
 		if (y < thirdOfHeight) {
