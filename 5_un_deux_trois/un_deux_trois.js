3304d7934c94aea9bd86db47ce2ebc6c5fe729ca
gergo225 <fazekasgergo225@gmail.com>
2021-09-23 19:08:48 +0300

    Setup for 'un deux trois' project. Add canvas with background

---
+++ 5_un_deux_trois/un_deux_trois.js
@@ -0,0 +1,11 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+let size = 480;
+let dpr = window.devicePixelRatio;
+canvas.width = size * dpr;
+canvas.height = size * dpr;
+context.scale(dpr, dpr);
+
+context.fillStyle = 'yellow';
+context.fillRect(0, 0, size, size);
