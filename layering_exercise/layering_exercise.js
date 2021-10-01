eff00fa06fcfb22a3296c6f77c73d3f4e9553c89
gergo225 <fazekasgergo225@gmail.com>
2021-10-01 14:25:52 +0300

    Create items with multiple layers. Add items to canvas randomly.

--- layering_exercise/layering_exercise.js
+++ layering_exercise/layering_exercise.js
@@ -8,18 +8,36 @@ canvas.height = dpr * size;
 context.scale(dpr, dpr);
 
 
-let layers = 10;
-for (let layer = 0; layer < layers; layer++) {
-	let redness = (224 - 17 * layer);
-	let opacity = 0.07 + 0.15 * (1 - (layer / layers));
-	context.fillStyle = 'rgba(' + redness + ', 56, 182, ' + opacity + ')';
 
-	let margin = layer * 10 + 48;
-	let rectSize = size - margin * 2;
-	let offset = 80 * Math.random() - 40;
-	context.fillRect(margin + offset, margin + offset, rectSize, rectSize);
+function drawLayeredSquares(x, y, areaSize) {
+	context.save();
 
-	context.translate(size / 2, size / 2);
-	context.rotate(Math.PI * Math.random());
-	context.translate(-size / 2, -size / 2);
+	let rectSize = areaSize;
+	let rotation = Math.floor(180 * Math.random());
+	let layers = 10;
+	for (let layer = 0; layer < layers; layer++) {
+		let redness = (224 - 17 * layer);
+		let opacity = 0.07 + 0.15 * (1 - (layer / layers));
+		context.fillStyle = 'rgba(' + redness + ', 56, 182, ' + opacity + ')';
+
+
+		context.translate(x, y);
+		context.rotate(Math.PI / 180 * rotation);
+
+		// let offset = areaSize / 4 * Math.random() - areaSize / 8;
+		// context.translate(offset, offset);
+		context.fillRect(-rectSize / 2, -rectSize / 2, rectSize, rectSize);
+		// context.translate(-offset, -offset);
+
+		context.translate(-x, -y);
+
+		rectSize -= rectSize / (layers - 1);
+		rotation += 2;
+	}
+	context.restore();
+}
+
+let itemCount = 4 + Math.floor(Math.random() * 6);
+for (let i = 0; i < itemCount; ++i) {
+	drawLayeredSquares(Math.random() * size, Math.random() * size, 20 + Math.random() * 80);
 }
