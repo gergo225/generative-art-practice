1136d9bfb7ac5a4dde2618311bcf07cb763f3ac9
gergo225 <fazekasgergo225@gmail.com>
2021-10-01 18:40:44 +0300

    Fix scaling

--- 6_circle_packing/circle_packing.js
+++ 6_circle_packing/circle_packing.js
@@ -5,6 +5,7 @@ let size = 480;
 let dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
+context.scale(dpr, dpr);
 
 context.fillStyle = 'purple';
 context.fillRect(0, 0, size, size);
