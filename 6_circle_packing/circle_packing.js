640171e353eb64de32bf426768941480f7ca166e
gergo225 <fazekasgergo225@gmail.com>
2021-10-01 18:46:06 +0300

    Add circles that don't collide with each other

--- 6_circle_packing/circle_packing.js
+++ 6_circle_packing/circle_packing.js
@@ -11,3 +11,52 @@ context.fillStyle = 'purple';
 context.fillRect(0, 0, size, size);
 
 context.lineWidth = 2;
+
+
+let circles = [];
+let minRadius = 2;
+let maxRadius = 100;
+let totalCircles = 500;
+let createCircleAttempts = 500;
+
+context.strokeStyle = 'magenta';
+
+function createAndDrawCircle() {
+	let newCircle = {
+		x: Math.floor(Math.random() * size),
+		y: Math.floor(Math.random() * size),
+		radius: minRadius
+	}
+
+	for (let radiusSize = minRadius; radiusSize <= maxRadius; radiusSize++) {
+		newCircle.radius = radiusSize;
+		if (doesCircleHaveACollision(newCircle)) {
+			newCircle.radius--;
+			break;
+		}
+	}
+
+	circles.push(newCircle);
+	context.beginPath();
+	context.arc(newCircle.x, newCircle.y, newCircle.radius, 0, Math.PI * 2);
+	context.stroke();
+}
+
+function doesCircleHaveACollision(circle) {
+	for (let i = 0; i < circles.length; ++i) {
+		let otherCircle = circles[i];
+		let combinedRadii = circle.radius + otherCircle.radius;
+		let x = circle.x - otherCircle.x;
+		let y = circle.y - otherCircle.y;
+
+		if (combinedRadii >= Math.sqrt(x * x + y * y)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+for (let i = 0; i < totalCircles; ++i) {
+	createAndDrawCircle();
+}
