fe2f488c72c59c1150983c92af2ab7bf79ceb6ce
gergo225 <fazekasgergo225@gmail.com>
2021-10-01 17:11:15 +0300

    Setup for 'circle packing' project. Add canvas with background

---
+++ 6_circle_packing/circle_packing.js
@@ -0,0 +1,12 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+let size = 480;
+let dpr = window.devicePixelRatio;
+canvas.width = size * dpr;
+canvas.height = size * dpr;
+
+context.fillStyle = 'purple';
+context.fillRect(0, 0, size, size);
+
+context.lineWidth = 2;
