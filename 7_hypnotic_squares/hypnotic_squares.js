bfcf30e445ae4e4896301ef99665cc2f34516577
gergo225 <fazekasgergo225@gmail.com>
2021-10-03 18:11:00 +0300

    Draw one square with smaller concentric squares using recursion

--- 7_hypnotic_squares/hypnotic_squares.js
+++ 7_hypnotic_squares/hypnotic_squares.js
@@ -7,5 +7,27 @@ canvas.width = size * dpr;
 canvas.height = size * dpr;
 context.scale(dpr, dpr);
 
-context.fillStyle = 'green';
+context.fillStyle = 'lightgreen';
 context.fillRect(0, 0, size, size);
+
+context.lineWidth = 2;
+context.strokeStyle = 'orange';
+
+const finalSize = 10;
+const startSize = size;
+const startSteps = 5;
+
+function draw(x, y, width, height, xMovement, yMovement, steps) {
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.stroke();
+
+	if (steps >= 0) {
+		const newSize = startSize * (steps / startSteps) + finalSize;
+		const newX = x + (width - newSize) / 2;
+		const newY = y + (height - newSize) / 2;
+		draw(newX, newY, newSize, newSize, xMovement, yMovement, steps - 1);
+	}
+}
+
+draw(0, 0, startSize, startSize, 0, 0, startSteps);
